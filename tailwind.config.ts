8acb28b571168ca7b38e985f1d445fa77c03e3b6
Shukur Huseynli <bjksukur@gmail.com>
2024-03-10 10:39:07 +0400

    add NextUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,13 @@
 import type { Config } from "tailwindcss"
+import {nextui} from "@nextui-org/react";
 
 const config = {
-  darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
 	],
   prefix: "",
   theme: {
@@ -74,7 +75,8 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  darkMode: "class",
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography"), nextui()],
 } satisfies Config
 
 export default config
