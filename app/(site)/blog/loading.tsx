bbb94dba2f62642bfa6b95007d24280c32f67412
Shukur Huseynli <bjksukur@gmail.com>
2024-03-10 10:43:14 +0400

    add Loading state

--- app/(site)/blog/loading.tsx
+++ app/(site)/blog/loading.tsx
@@ -2,7 +2,7 @@ import {CircularProgress} from "@nextui-org/react";
 
 export default function Loading(){
     return (
-        <main className="min-h-screen w-full flex items-center">
+        <main className="min-h-screen w-full flex justify-center items-center">
             <CircularProgress classNames={{
                 track:"bg-[#33e]"
             }} size="lg" aria-label="Loading..." />
