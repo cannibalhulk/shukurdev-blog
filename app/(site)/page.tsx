34ab3e1dcec43a4adabd26f06c05ad7eaee11cc0
Shukur Huseynli <bjksukur@gmail.com>
2024-03-14 22:31:47 +0400

    nothing important

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -22,30 +22,33 @@ async function getData() {
 export default async function Home() {
   const data: BlogCard[] = await getData();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
-      {data.map((blog, i) => (
-        <Card key={i} className="drop-shadow-xl shadow-slate-400">
-          <Image
-          className="rounded-t-lg h-[200px] object-cover"
-            width={800}
-            height={500}
-            src={urlFor(blog.titleImage).url()}
-            alt="blog_image"
-          />
+    <>
 
-          <CardContent className="mt-5">
-            <h4 className="text-[13px] mb-2 text-neutral-400"><span className="font-bold">Created at: </span>{dayjs(blog._createdAt).format("MMM DD, YYYY HH:mm")}</h4>
-            <h3 className="text-lg line-clamp-2 font-bold">{blog.title}</h3>
-            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">{blog.smallDescription}</p>
-            <Button asChild className="w-full mt-7">
-              <Link href={`/blog/${blog.currentSlug}`}>Read More</Link>
-            </Button>
-          </CardContent>
-        </Card>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+        {data.map((blog, i) => (
+          <Card key={i} className="drop-shadow-xl shadow-slate-400">
+            <Image
+            className="rounded-t-lg h-[200px] object-cover"
+              width={800}
+              height={500}
+              src={urlFor(blog.titleImage).url()}
+              alt="blog_image"
+            />
+
+            <CardContent className="mt-5">
+              <h4 className="text-[13px] mb-2 text-neutral-400"><span className="font-bold">Created at: </span>{dayjs(blog._createdAt).format("MMM DD, YYYY HH:mm")}</h4>
+              <h3 className="text-lg line-clamp-2 font-bold">{blog.title}</h3>
+              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">{blog.smallDescription}</p>
+              <Button asChild className="w-full mt-7">
+                <Link href={`/blog/${blog.currentSlug}`}>Read More</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
       <br/>
       <br/>
       <br/>
-    </div>
+    </>
   );
 }
