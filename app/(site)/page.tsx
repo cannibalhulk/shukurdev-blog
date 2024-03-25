550cdab912d351ca477ce8e93ae5fc73787da7e7
Shukur Huseynli <bjksukur@gmail.com>
2024-03-25 18:52:21 +0400

    add CTA button

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 import { client, urlFor } from "@/lib/sanity";
 import { BlogCard } from "@/types/interface";
 import { Card, CardContent } from "@/components/ui/card";
-import Link from "next/link";
-import {useRouter} from "next/navigation";
-import { Button } from "@/components/ui/button";
 import dayjs from "dayjs";
+import CTAButton from "@/components/CTA-Button";
 
 async function getData() {
   const query = `*[_type=='blog'] | order(_createdAt desc) {
@@ -22,18 +20,17 @@ async function getData() {
 
 export default async function Home() {
   const data: BlogCard[] = await getData();
-const router = useRouter();
-  function onClick(link:string){
-   router.push(link,{scroll:false})
-  }
+  
   return (
     <>
-
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
         {data.map((blog, i) => (
-          <Card key={i} className="drop-shadow-xl dark:drop-shadow-none dark:shadow-none shadow-slate-400">
+          <Card
+            key={i}
+            className="drop-shadow-xl dark:drop-shadow-none dark:shadow-none shadow-slate-400"
+          >
             <Image
-            className="rounded-t-lg h-[200px] object-cover"
+              className="rounded-t-lg h-[200px] object-cover"
               width={800}
               height={500}
               src={urlFor(blog.titleImage).url()}
@@ -41,19 +38,26 @@ const router = useRouter();
             />
 
             <CardContent className="mt-5">
-              <h4 className="text-[13px] mb-2 text-neutral-400"><span className="font-bold">Created at: </span>{dayjs(blog._createdAt).format("MMM DD, YYYY HH:mm")}</h4>
+              <h4 className="text-[13px] mb-2 text-neutral-400">
+                <span className="font-bold">Created at: </span>
+                {dayjs(blog._createdAt).format("MMM DD, YYYY HH:mm")}
+              </h4>
               <h3 className="text-lg line-clamp-2 font-bold">{blog.title}</h3>
-              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">{blog.smallDescription}</p>
-              <Button onClick={()=>onClick(`/blog/${blog.currentSlug}`)} className="w-full mt-7">
+              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+                {blog.smallDescription}
+              </p>
+              <CTAButton
+              link={`/blog/${blog.currentSlug}`}
+              >
                 Read More
-              </Button>
+              </CTAButton>
             </CardContent>
           </Card>
         ))}
       </div>
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
     </>
   );
 }
---
+++ components/CTA-Button.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+
+function CTAButton( {children, link} : {
+    children: React.ReactNode,
+    link: string
+}) {
+    const router = useRouter();
+  return ( 
+    <Button
+        className="w-full mt-7"
+        onClick={() => router.push(link, {scroll:false})}
+    >
+        {children}
+    </Button>
+  )
+}
+
+export default CTAButton
