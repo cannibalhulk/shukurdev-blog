9fb635f6e8a79a667dab6dee30877b2c9c04aabe
Shukur Huseynli <bjksukur@gmail.com>
2024-03-26 20:21:03 +0400

    setup GTM

--- app/(search)/layout.tsx
+++ app/(search)/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import { Metadata } from "next/types";
+import { GoogleTagManager } from "@next/third-parties/google";
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,38 +10,16 @@ export const metadata: Metadata = {
   description: "Weekly technical blogs and articles",
 };
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K2NJL28S');</script>
-        <!-- End Google Tag Manager -->
-      </head>
+      <GoogleTagManager gtmId="GTM-K2NJL28S" />
 
       <body className={inter.className}>
-      <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2NJL28S"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-        <ThemeProvider
-          defaultTheme="system"
-          attribute="class"
-          enableSystem
-        >
-          <main className="max-w-2xl mx-auto px-4 pt-4">
-            {children}
-          </main>
+        <ThemeProvider defaultTheme="system" attribute="class" enableSystem>
+          <main className="max-w-2xl mx-auto px-4 pt-4">{children}</main>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Navbar";
+import { GoogleTagManager } from '@next/third-parties/google'
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,20 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-            <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-K2NJL28S');</script>
-      <!-- End Google Tag Manager -->
-      </head>
+      <GoogleTagManager gtmId="GTM-K2NJL28S" />
+      
       <body className={inter.className}>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2NJL28S"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
         <ThemeProvider
           defaultTheme="system"
           attribute="class"
